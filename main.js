df18ed044bde85b9e0773da39cca21d9f68d5a65
AleksDev <lesha.rudiko@gmail.com>
2023-09-15 20:26:30 +0300

    update

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-
 const sendData = (data) => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: "POST", 
-        body: JSON.stringify(data),
-        headers:{
-            'Content-Type': 'application/json;charset=utf-8'
-        }
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  })
+    .then((r) => {
+      if (r.ok === true) alert("данные успешно отправлены");
     })
-        .then((r => {if(r.ok === true)alert('данные успешно отправлены')}))
-        .catch((error => alert("Упс, произошла ошибка" + error)))
-}
+    .catch((error) => alert("Упс, произошла ошибка" + error));
+};
 
 const getData = () => {
-    fetch('db.json')
-        .then((r => r.json()))
-        .then((data => sendData(data)))
-}
+  fetch("db.json")
+    .then((r) => r.json())
+    .then((data) => sendData(data));
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-    getData()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  getData();
+});
